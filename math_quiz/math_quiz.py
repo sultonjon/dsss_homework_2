e9cc5af04fc97a746d79fb8510dba916f78431b4
sultonjon <sulton.iskhakov@fau.de>
2023-11-11 13:57:22 +0100

    math_quiz.py and tests_math.py files were changed and added to code_cleanup branch

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,87 @@
 import random
 
 
-def function_A(min, max):
+def random_integer_generator(min_int, biggest_int):
     """
-    Random integer.
+    Generates a random integer number within the range of smallest_int and biggest_int and returns the generated number.
+    Including smallest_int but excluding bigger_int
+
+    min_int: Minimal integer value for random integer generation
+    type min_int: int
+    biggest_int: biggest integer number for the int generation excluding biggest_int
+    type biggest_int: int
     """
-    return random.randint(min, max)
+    try:
+        random_num = random.randint(min_int, biggest_int)
+        return random_num
+    except TypeError:
+        print( "One or both of your entered number/s is/are not integer/s")
 
 
-def function_B():
+def sign_generator():
+
+    # Randomly selects and returns one of the array elements (Operator)
+
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def math_operation(number1, number2, operator):
+
+    """
+    performs some math operations such as addition, subtraction, and multiplication
+
+    number1: Given first number
+    type number1: float, int
+    number2: Given second number
+    type number2: float, int
+    operator: mathematical operator
+    type operator: string
+    problem: Mathematical problem
+    type problem: string
+    """
+
+    problem = f"{number1} {operator} {number2}"
+    if operator == '+':
+        answer = number1 + number2
+    elif operator == '-':
+        answer = number1 - number2
+    else:
+        answer = number1 * number2
+    return problem, answer
+
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Small math quiz program which generates randomly two numbers with random
+    operation and tests the user. When the user answers correctly his score increases otherwise the game
+    finishes and shows the score of the user
 
+    score: Number of correctly answered questions
+    type score: int
+    total_questions: Number of total questions
+    type total_questions:
+    """
+
+    score = 0
+    total_questions = 3  #3.14159265359
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
-
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
-
-        if useranswer == ANSWER:
+    for _ in range(total_questions):
+        number1 = random_integer_generator(1, 10)
+        number2 = random_integer_generator(1, 5)
+        operator = sign_generator()
+        problem, answer = math_operation(number1, number2, operator)
+        print(f"\nQuestion: {problem}")
+        user_answer = input("Your answer: ")
+        user_answer = int(user_answer)
+        if user_answer == answer:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += -(-1)
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {answer}.")
+        print(f"\nGame over! Your score is: {score}/{total_questions}")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
 
 if __name__ == "__main__":
     math_quiz()
--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,30 +1,36 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import random_integer_generator, sign_generator, math_operation
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_random_integer_generator(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
         for _ in range(1000):  # Test a large number of random values
-            rand_num = function_A(min_val, max_val)
+            rand_num = random_integer_generator(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_sign_generator(self):
+        all_signs = ['+', '-', '*']
+        for _ in range(1000):
+            sign = sign_generator()
+            self.assertIn(sign, all_signs)
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
-            ]
+    def test_math_operation(self):
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),
+            (8, 3, '-', '8 - 3', 5),
+            (1, 9, '*', '1 * 9', 9)
+        ]
+
+        for num1, num2, operator, expected_problem, expected_answer in test_cases:
+            problem, real_answer = math_operation(num1, num2, operator)
+            self.assertTrue(problem == expected_problem)
+            self.assertTrue(real_answer == expected_answer)
+
 
 if __name__ == "__main__":
     unittest.main()
